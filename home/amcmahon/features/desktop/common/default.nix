e0d8324e9178ec40671afba04f27e530d7f0d26c
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2023-04-08 11:36:29 +0100

    Audio

--- home/amcmahon/features/desktop/common/default.nix
+++ home/amcmahon/features/desktop/common/default.nix
@@ -2,9 +2,14 @@
 {
   imports = [
     ./chromium.nix
+    ./dragon.nix
     ./firefox.nix
     ./font.nix
     ./gtk.nix
+    ./dragon.nix
+    ./pavucontrol.nix
+    ./playerctl.nix
+    ./qt.nix
   ];
 
   xdg.mimeApps.enable = true;
