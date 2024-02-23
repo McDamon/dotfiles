286afd69acf03366ac4ba6dc21db8e6ff4c937ba
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-02-23 23:25:27 +0000

    Added libvirt

--- hosts/common/global/libvirtd.nix
+++ hosts/common/global/libvirtd.nix
@@ -1,13 +1,19 @@
-{ pkgs }:
+{ pkgs, ... }:
 {
   programs.virt-manager.enable = true;
 
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu.ovmf.enable = true;
-    qemu.ovmf.package = [ pkgs.OVMFFull ];
-    qemu.swtpm.enable = true;
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
   };
+  services.spice-vdagentd.enable = true;
 
   environment.sessionVariables.LIBVIRT_DEFAULT_URI = [ "qemu:///system" ];
 }
