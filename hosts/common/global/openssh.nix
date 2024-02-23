7d0106effd4bbf2b856f1bf9641d3259dfd54458
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-02-23 21:58:14 +0000

    Added ssh pub key

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -18,6 +18,10 @@
     }];
   };
 
+  users.users."amcmahon".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAtdAp9hxzhMNP1NgEGnwkHURfm9Z93p0vL2HG9wRDSu andre@rocinante"
+  ];
+
   # Passwordless sudo when SSH'ing with keys
   security.pam.sshAgentAuth.enable = true;
 }
