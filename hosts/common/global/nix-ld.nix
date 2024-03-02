e4e364ebd71e315c1c4a895064f0ab98a5e63948
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-03-02 12:36:42 +0000

    Test 2

--- hosts/common/global/nix-ld.nix
+++ hosts/common/global/nix-ld.nix
@@ -1,7 +1,10 @@
-{ nix-ld-rs }:
+{ pkgs, nix-ld-rs ? null }:
+let 
+  nix-ld-rs' = if nix-ld-rs != null then nix-ld-rs else pkgs.nix-ld-rs;
+in 
 {
   programs.nix-ld = {
     enable = true;
-    package = nix-ld-rs;
+    package = nix-ld-rs';
   };
 }
