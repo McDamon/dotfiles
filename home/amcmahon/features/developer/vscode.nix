846ec72429875c6fe6a57b6a5355db9d06841943
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-05-04 15:13:12 +0100

    Vscode mods

--- home/amcmahon/features/developer/vscode.nix
+++ home/amcmahon/features/developer/vscode.nix
@@ -5,7 +5,7 @@
     package = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: {
       src = (builtins.fetchTarball {
         url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
-        sha256 = "sha256:0q2i189rqyi6m6j0jxhg6z6nb5l6kf9sl51cjhjc4y1s8ayfkcz3";
+        sha256 = "sha256:0kcnis32gnclbzvcbinvdl2abgbjisqkzg9h6p97vsswaz2y8wd3";
       });
       version = "latest";
     });
@@ -27,26 +27,26 @@
       {
         name = "cpptools";
         publisher = "ms-vscode";
-        version = "1.20.2";
+        version = "1.20.4";
         sha256 = "sha256-wpaSzYNwMGZ6T1LFbiwpF5koY5y1MDQuNXBosMn12cY=";
       }
       {
         name = "cmake-tools";
         publisher = "ms-vscode";
-        version = "1.18.30";
+        version = "1.18.33";
         sha256 = "sha256-KDTipUL/Vbd1PJ0GMrUYjWJ6XAW221aRxAIPGdBInQw=";
       }
       {
         name = "python";
         publisher = "ms-python";
-        version = "2024.5.11141012";
+        version = "	2024.7.11241010";
         sha256 = "sha256-lwjozafL3IQwxmFTzA2OFQo1bC3NCoBKPrr3kpgASMY=";
       }
       {
         name = "debugpy";
         publisher = "ms-python";
-        version = "2024.5.11141010";
-        sha256 = "sha256-TFo+/SISPFacRcHMvz5aBP85j3MYwkG+4zBzt3vk4AI=";
+        version = "	2024.6.0";
+        sha256 = "sha256-DwPlxnZlwhnT8cdzYd7N52i3SisXUc9WSs00SdNMuhc=";
       }
     ];
 
