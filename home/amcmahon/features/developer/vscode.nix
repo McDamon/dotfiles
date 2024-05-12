c4f101cebd3b21b25a478ca50de658147b5cb9e6
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-05-12 17:00:40 +0100

    More vscode fixes

--- home/amcmahon/features/developer/vscode.nix
+++ home/amcmahon/features/developer/vscode.nix
@@ -2,13 +2,13 @@
   programs.vscode = {
     enable = true;
 
-    package = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: {
-      src = (builtins.fetchTarball {
-        url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
-        sha256 = "sha256:0q2i189rqyi6m6j0jxhg6z6nb5l6kf9sl51cjhjc4y1s8ayfkcz3";
-      });
-      version = "latest";
-    });
+    #package = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: {
+    #  src = (builtins.fetchTarball {
+    #    url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
+    #    sha256 = "sha256:0q2i189rqyi6m6j0jxhg6z6nb5l6kf9sl51cjhjc4y1s8ayfkcz3";
+    #  });
+    #  version = "latest";
+    #});
 
     extensions = with pkgs.vscode-extensions; [
       zxh404.vscode-proto3
@@ -24,31 +24,12 @@
       rust-lang.rust-analyzer
       vadimcn.vscode-lldb
       github.vscode-github-actions
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "cpptools";
-        publisher = "ms-vscode";
-        version = "1.20.5";
-        sha256 = "sha256-tCN8+vRItvMf93XoZMZc2CcrKHjXMBT/4RfGV6JFKsg=";
-      }
-      {
-        name = "cmake-tools";
-        publisher = "ms-vscode";
-        version = "1.18.33";
-        sha256 = "sha256-CAIzeOx1nprn2AUp4ND4dTXACiV40xgHyS/vcuOmeus=";
-      }
-      {
-        name = "python";
-        publisher = "ms-python";
-        version = "2024.7.11241010";
-        sha256 = "sha256-ZbHTJBKSXkJ1abX1EnXKnFnPvKPhI1U+qkhRyyBkvr4=";
-      }
-      {
-        name = "debugpy";
-        publisher = "ms-python";
-        version = "2024.6.0";
-        sha256 = "sha256-VlPe65ViBur5P6L7iRKdGnmbNlSCwYrdZAezStx8Bz8=";
-      }
+      github.copilot
+      github.copilot-chat
+      ms-vscode.cpptools
+      ms-vscode.cmake-tools
+      ms-python.python
+      ms-python.debugpy
     ];
 
     userSettings = {
