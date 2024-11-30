4d75bccb0c71717ebfd8ef411250a67c3d1fd3e9
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2024-11-30 14:34:13 +0000

    Fix SSH perms on config

--- home/amcmahon/features/tools/ssh.nix
+++ home/amcmahon/features/tools/ssh.nix
@@ -10,4 +10,9 @@ in
           IdentityAgent ${onePassPath}
     '';
   };
+
+  home.file.".ssh/config" = {
+    target = ".ssh/config_source";
+    onChange = ''cat .ssh/config_source > .ssh/config && chmod 400 .ssh/config'';
+  };
 }
