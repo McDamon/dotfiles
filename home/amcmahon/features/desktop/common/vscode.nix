78797d366a69be80e929537340ecf3f1ee234fb7
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2025-11-01 20:36:20 +0000

    WIP

--- home/amcmahon/features/desktop/common/vscode.nix
+++ home/amcmahon/features/desktop/common/vscode.nix
@@ -3,5 +3,12 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode.fhs;
+    profiles.default.userSettings = {
+      # Prevent opening in new workspace
+      "window.openFoldersInNewWindow" = "off";
+      "window.openFilesInNewWindow" = "off";
+      # Don't restore previous windows/workspaces
+      "window.restoreWindows" = "none";
+    };
   };
 }
